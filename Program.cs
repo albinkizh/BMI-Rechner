f5f707f70e2ac2c66b7cc79ac628af977762478d
Crypet <chsu@test.at>
2023-04-20 08:51:03 +0200

    updated

--- Program.cs
+++ Program.cs
@@ -18,30 +18,31 @@
         {
             if (bmi < 18.5)
             {
-                Console.WriteLine("Du gehörst zur Untergewichtigten Gruppe");
+                Console.WriteLine("Sie gehören zur Untergewichtigten Gruppe");
             }
             else if (bmi < 24.9)
             {
-                Console.WriteLine("Du gehörst zur Normalgewichtingten Gruppe");
+                Console.WriteLine("Sie gehören zur Normalgewichtingten Gruppe");
             }
             else if (bmi < 29.9)
             {
-                Console.WriteLine("Du gehörst zur Übergewichtigten Gruppe");
+                Console.WriteLine("Sie gehören zur Übergewichtigten Gruppe");
             }
             else if (bmi < 34.9)
             {
-                Console.WriteLine("Du gehörst zur starken Übergewichtigten Gruppe");
+                Console.WriteLine("Sie gehören zur starken Übergewichtigten Gruppe");
             }
             else
             {
-                Console.WriteLine("Du gehörst zur Adipositas Gruppe");
+                Console.WriteLine("Sie gehören zur Adipositas Gruppe");
             }
 
             Console.WriteLine("Nochmal? (Ja/Nein)");
             string asn = Console.ReadLine();
             if (asn == "Ja")
             {
-                Console.WriteLine("Willkommen zurück");
+                Console.Clear();
+                Console.WriteLine("Willkommen zurück!");
                 continue;
             }
             else
@@ -62,35 +63,36 @@
 
         double bmi = Gewicht / (Größe * Größe);
 
-        Console.WriteLine(bmi);
+        Console.WriteLine("Ihr Bmiwert ist: " + bmi);
 
         {
             if (bmi < 17.5)
             {
-                Console.WriteLine("Du gehörst zur Untergewichtigten Gruppe");
+                Console.WriteLine("Sie gehören zur Untergewichtigten Gruppe");
             }
             else if (bmi < 24)
             {
-                Console.WriteLine("Du gehörst zur Normalgewichtingten Gruppe");
+                Console.WriteLine("Sie gehören zur Normalgewichtingten Gruppe");
             }
             else if (bmi < 29)
             {
-                Console.WriteLine("Du gehörst zur Übergewichtigten Gruppe");
+                Console.WriteLine("Sie gehören zur Übergewichtigten Gruppe");
             }
             else if (bmi < 34)
             {
-                Console.WriteLine("Du gehörst zur starken Übergewichtigten Gruppe");
+                Console.WriteLine("Sie gehören zur starken Übergewichtigten Gruppe");
             }
             else
             {
-                Console.WriteLine("Du gehörst zur Adipositas Gruppe");
+                Console.WriteLine("Sie gehören zur Adipositas Gruppe");
             }
 
             Console.WriteLine("Nochmal? (Ja/Nein)");
             string asn = Console.ReadLine();
             if (asn == "Ja")
             {
-                Console.WriteLine("Willkommen zurück");
+                Console.Clear();
+                Console.WriteLine("Willkommen zurück!");
                 continue;
             }
             else
@@ -116,30 +118,31 @@
         {
             if (bmi < 14.4)
             {
-                Console.WriteLine("Du gehörst zur Untergewichtigten Gruppe");
+                Console.WriteLine("Sie gehören zur Untergewichtigten Gruppe");
             }
             else if (bmi < 20.8)
             {
-                Console.WriteLine("Du gehörst zur Normalgewichtingten Gruppe");
+                Console.WriteLine("Sie gehören zur Normalgewichtingten Gruppe");
             }
             else if (bmi < 24.2)
             {
-                Console.WriteLine("Du gehörst zur Übergewichtigten Gruppe");
+                Console.WriteLine("Sie gehören zur Übergewichtigten Gruppe");
             }
             else if (bmi < 25)
             {
-                Console.WriteLine("Du gehörst zur starken Übergewichtigten Gruppe");
+                Console.WriteLine("Sie gehören zur starken Übergewichtigten Gruppe");
             }
             else
             {
-                Console.WriteLine("Du gehörst zur Adipositas Gruppe");
+                Console.WriteLine("Sie gehören zur Adipositas Gruppe");
             }
 
             Console.WriteLine("Nochmal? (Ja/Nein)");
             string asn = Console.ReadLine();
             if (asn == "Ja")
             {
-                Console.WriteLine("Willkommen zurück");
+                Console.Clear();
+                Console.WriteLine("Willkommen zurück!");
                 continue;
             }
             else
